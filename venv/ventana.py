09c26f7cc09fc7bf8b7217af4d88e52f4491703d
MarsPatrick <xxaltairxx@hotmail.cl>
2021-04-02 17:36:12 -0300

    Se cambio de main a ventana Se agregaron acciones de ventanas Usar python 3.9

---
+++ venv/ventana.py
@@ -0,0 +1,29 @@
+from tkinter import Tk, Label, Button, messagebox
+
+#Una accion generica
+def accion():
+    print("Lo que hara")
+
+#VentanaEmergente(Info)
+def VEI():
+    messagebox.showinfo("Hola","holo como estas tu")
+
+
+#Declaracion de la ventana
+ventana = Tk()
+
+#Tamaño de la ventana
+ventana.geometry("600x480")
+
+#Titulo de la ventana
+ventana.title("Titulo")
+
+#Label
+lbl = Label(ventana, text='El label')
+lbl.pack()
+
+#Button
+btn = Button(ventana, text='El boton', command=VEI)
+btn.pack()
+
+ventana.mainloop()
