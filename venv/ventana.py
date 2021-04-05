e0bf560d80b3dd1cb71a2877cd4edbe3e9a8b583
MarsPatrick <xxaltairxx@hotmail.cl>
2021-04-05 16:37:36 -0400

    Modificando ui nuevos botones previsualizacion dentro de la interfaz

--- venv/ventana.py
+++ venv/ventana.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import messagebox
-
+from PIL import Image
+from PIL import ImageTk
+import cv2
+import imutils
 
 # Una accion generica
 def accion():
@@ -9,27 +12,72 @@ def accion():
 
 
 # VentanaEmergente(Info)
-def VEI():
-    messagebox.showinfo("Hola", "holaaa como estas tu")
-    import camara
+def vei():
+    print("Lo que hara")
+
+def iniciar():
+    global cap
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    visualizar()
+
+def visualizar():
+    global cap
+    if cap is not None:
+        ret, frame = cap.read()
+        if ret == True:
+            frame = imutils.resize(frame, width=640)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            im = Image.fromarray(frame)
+            img = ImageTk.PhotoImage(image=im)
+            lblVideo.configure(image=img)
+            lblVideo.image = img
+            lblVideo.after(10, visualizar)
+        else:
+            lblVideo.image = ""
+            cap.release()
+
+def finalizar():
+    global cap
+    cap.release()
 
 
 # Declaracion de la ventana
 ventana = Tk()
 
+# Create left and right frames
+left_frame = Frame(ventana, width=200, height=400, bg='grey')
+left_frame.grid(row=0, column=0, padx=10, pady=5)
+
+mid_frame = Frame(ventana, width=650, height=400, bg='grey')
+mid_frame.grid(row=0, column=1, padx=10, pady=5)
+
+right_frame = Frame(ventana, width=650, height=400, bg='grey')
+right_frame.grid(row=0, column=2, padx=10, pady=5)
+
 # Tamaño de la ventana
-ventana.geometry("600x480")
+ventana.geometry("900x500")
 
 # Titulo de la ventana
 ventana.title("Titulo")
 
-# Label
-lbl = Label(ventana, text='El label')
-lbl.pack(side=TOP)
+# Button
+btn0 = Button(left_frame, text='Ver previa', command=iniciar)
+btn0.grid(column=0, row=0, padx=5, pady=5)
 
 # Button
-btn = Button(ventana, text='El boton', command=VEI)
-btn.pack(side=BOTTOM)
+btn1 = Button(left_frame, text='Ocultar previa', command=finalizar)
+btn1.grid(column=0, row=1, padx=5, pady=5)
 
+# Button
+btn2 = Button(right_frame, text='Tomar foto', command=vei)
+btn2.grid(column=0, row=0, padx=5, pady=5)
+
+# Button
+btn3 = Button(right_frame, text='Tomar foto 360', command=vei)
+btn3.grid(column=0, row=1, padx=5, pady=5)
+
+# Video
+lblVideo= Label(mid_frame)
+lblVideo.grid(column=0, row=0, columnspan=2)
 
 ventana.mainloop()
