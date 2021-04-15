185929675f1f57603fa7d560115614fe96154f9d
MarsPatrick <altair.aguila41@gmail.com>
2021-04-15 16:36:59 -0400

    probando el detector con mas objetos

--- detector.py
+++ detector.py
@@ -3,14 +3,14 @@ import cv2
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
 objetoClassif1 = cv2.CascadeClassifier('cascade.xml')
-objetoClassif2 = cv2.CascadeClassifier('cascade.xml')
+objetoClassif2 = cv2.CascadeClassifier('cascade1.xml')
 while True:
 
         ret,frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        objeto1 = objetoClassif1.detectMultiScale(gray, scaleFactor=5, minNeighbors=91,minSize=(70,78))
-        objeto2 = objetoClassif1.detectMultiScale(gray, scaleFactor=5, minNeighbors=91, minSize=(70, 78))
+        objeto1 = objetoClassif1.detectMultiScale(gray, scaleFactor=4, minNeighbors=91,minSize=(70,70))
+        objeto2 = objetoClassif2.detectMultiScale(gray, scaleFactor=2, minNeighbors=50, minSize=(70, 78))
 
         for (x,y,w,h) in objeto1:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
